f61cd3ba44c18ec3134fbc234cc3377ddebcf7b4
bzakrzewski <bzakrzewski@griddynamics.com>
2024-10-02 15:56:03 +0200

    reformat: Optimize validators and util functions

--- util.ts
+++ util.ts
@@ -14,26 +14,30 @@ export const getResponseWhenServerFailed = (res: Response) => {
   });
 };
 
-export const isNumber = (value: any): boolean => {
-  return typeof value === 'number';
-};
-
-// https://stackoverflow.com/questions/175739/how-can-i-check-if-a-string-is-a-valid-number
-export function isNumeric(str: string): boolean {
-  return !isNaN(+str) && !isNaN(parseFloat(str));
+export function isNumeric(value: any): boolean {
+  return !isNaN(+value);
 }
 
-// https://stackoverflow.com/questions/18758772/how-do-i-validate-a-date-in-this-format-yyyy-mm-dd-using-jquery
-export function isValidDateYYYYMMDD(dateString: string): boolean {
-  const regEx = /^\d{4}-\d{2}-\d{2}$/;
-  if (!dateString.match(regEx)) {
-    return false;
-  }
+export const isStringContainingOnlyWhiteSpace = (str: string) => {
+  return !str.trim();
+};
 
+const isCorrectDate = (dateString: string) => {
   const d = new Date(dateString);
+
   const dNum = d.getTime();
   if (!dNum && dNum !== 0) {
     return false;
   }
   return d.toISOString().slice(0, 10) === dateString;
+};
+
+export function isValidDateYYYYMMDD(dateString: string): boolean {
+  const regEx = /^\d{4}-\d{2}-\d{2}$/;
+
+  if (!dateString.match(regEx)) {
+    return false;
+  }
+
+  return isCorrectDate(dateString);
 }
--- validators.ts
+++ validators.ts
@@ -1,8 +1,8 @@
 import { Response } from 'express';
-import { capitalizeFirstLetter, isNumeric, isValidDateYYYYMMDD } from './util';
+import { capitalizeFirstLetter, isNumeric, isStringContainingOnlyWhiteSpace, isValidDateYYYYMMDD } from './util';
 
 export const validateIfEmpty = (param: any, name: string, res: Response) => {
-  if (!param || !param.toString().trim()) {
+  if (!param || (typeof param === 'string' && isStringContainingOnlyWhiteSpace(param))) {
     return res.status(400).json({
       message: `No ${name} provided!`,
     });
@@ -12,13 +12,11 @@ export const validateIfEmpty = (param: any, name: string, res: Response) => {
 };
 
 export const validateIfPositiveNumber = (param: any, name: string, res: Response) => {
-  const paramStr = param.toString();
-
-  if (!isNumeric(paramStr)) {
+  if (!isNumeric(param)) {
     return res.status(400).json({
       message: `${capitalizeFirstLetter(name)} is in the wrong format!`,
     });
-  } else if (+paramStr <= 0) {
+  } else if (+param <= 0) {
     return res.status(400).json({
       message: `${capitalizeFirstLetter(name)} cannot be less or equal to 0!`,
     });
@@ -28,7 +26,7 @@ export const validateIfPositiveNumber = (param: any, name: string, res: Response
 };
 
 export const validateIfCorrectDateFormat = (param: any, name: string, res: Response) => {
-  if (param && !isValidDateYYYYMMDD(param.toString())) {
+  if (!isValidDateYYYYMMDD(param.toString())) {
     return res.status(400).json({
       message: `${capitalizeFirstLetter(name)} is in the wrong format!`,
     });
